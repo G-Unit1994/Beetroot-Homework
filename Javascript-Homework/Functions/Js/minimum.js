273e86d3350a08d121f6bac07d1f3cf0a11392e9
G-Unit1994 <161944853+G-Unit1994@users.noreply.github.com>
2024-05-18 14:58:58 +0300

    Update minimum.js

--- Javascript-Homework/Functions/Js/minimum.js
+++ Javascript-Homework/Functions/Js/minimum.js
@@ -1,86 +1,19 @@
-// 1) Напиши всі можливі варіанти створення функцій:
-// А) Створення функції без параметрів:
-function hello_world(){
-    console.log("Hello, world!");
-}
-    hello_world();  
+ 
 
-//  2) Створення функції з параметрами:
-function greet(name) {
-    console.log("Hello", + name + "!");
-}
-console.log("name");
-
-// 3) Створення функції з параметрами за замовчуванням:
-function greet(name = "Guest") {
-    console.log("Hello, " + name + "!");
-}
-
-console.log(); // Виклик функції без параметрів
-console.log("Guest"); // Виклик функції з параметром
-// 4) Повернення значення з функції:
-function addNumbers(a, b) {
-    return a + b;
-}
-
-let result = addNumbers(3, 5);
-console.log(result);
-
-// 5) Використання змінної кількості аргументів (*args та kwargs):
-function Calculatesum(...args) {
-    let total = 0;
-    for(let num of args) {
-        total += num;
-    }
-    return total;
-}
-
-let sum = Calculatesum(1,2,3,4,5);
-console.log(sum);
-// 6) Рекурсивнa функції:
-function calculateFactorial(n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * calculateFactorial(n - 1);
-    }
-}
-
-let factorialResult = calculateFactorial(5);
-console.log(factorialResult); // Виведе: 120
-// 7) Анонімнa функції (лямбда-функція):
-let square = function(x) {
-    return x ** 2;
+//  1) Функціональний вираз:
+function funcExpr = function() {
+    console.log("Функціональний вираз");
 };
 
-console.log(square(5)); // Виведе: 25
-// 8) Генератори функцій:
-function* countdown(n) {
-    while (n > 0) {
-        yield n;
-        n--;
-    }
+// B):
+function funcDecl() {
+    console.log("Оголошення функції");
 }
 
-let generator = countdown(5);
-for (let value of generator) {
-    console.log(value); // Виведе: 5, 4, 3, 2, 1
-}
-// 9) Декоратори для функцій:
-function my_decorator(func) {
-    return function() {
-        console.log("Something is happening before the function is called.");
-        func();
-        console.log("Something is happening after the function is called.");
-    };
-}
-
-function say_hello() {
-    console.log("Hello!");
-}
-
-let decorated_function = my_decorator(say_hello);
-decorated_function(); // Виклик декорованої функції
+// C:
+const arrowFunc = () => {
+    console.log("Стрілкова функція");
+};
 
 // 2) Створи функцію, яка буде виводити кількість переданих їй аргументів.
 function count_arguments(...args) {
@@ -100,9 +33,9 @@ function comparenumbers(num1, num2) {
     if(num1 < num2) {
         return -1;
     }else if (num1 > num2) {
-        return 2.1;
+        return 1;
     }else {
-        return 3.0;
+        return 0;
     }
 }
 
